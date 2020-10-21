beb4d6d6c57d575dd95aba2d3074060b43dbf298
HugoAdi <hugoadijm@gmail.com>
2020-10-21 15:22:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const video = document.getElementById('video'); 
+const button = document.getElementById('button');
+const button2 = document.getElementById('button2');
+const canvas = document.getElementById('canvas');
+
+const constraints = {
+    video: {
+      width: {
+        min: 1280,
+        ideal: 1920,
+        max: 2560,
+      },
+      height: {
+        min: 720,
+        ideal: 1080,
+        max: 1440,
+      },
+      facingMode: "environment"
+    }, 
+};
+
+function capturarFoto(){
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext("2d").drawImage(video, 0, 0);
+}
+
+function navegador(){
+    if (
+    !"mediaDevices" in navigator ||
+    !"getUserMedia" in navigator.mediaDevices
+    ) {
+        alert("Camera API is not available in your browser");
+        return;
+    }
+}
+
+async function inicializarCamara() {
+    //stopVideoStream();
+    //constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+    try {
+        videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = videoStream;
+    } catch (err) {
+        alert("Could not access the camera");
+    }
+}
+
+button.addEventListener('click',event =>{
+    console.log("aiuda");
+    navegador();
+    inicializarCamara();
+});
+
+button2.addEventListener('click',event =>{
+    capturarFoto();
+});
+
